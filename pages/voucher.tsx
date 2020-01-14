d2cf3c0c4187874eb9889b1e4658e7494805f341
wayne <wjmpantig@gmail.com>
2020-01-14 14:05:43 +0800

    lint fix

--- pages/voucher.tsx
+++ pages/voucher.tsx
@@ -1,8 +1,14 @@
+import React, { FunctionComponent } from 'react';
 import Error from 'next/error';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_OFFER_BY_SLUG } from '../apollo/offer';
 
-const Voucher = ({ slug, id}) =>{
+type VoucherProps = {
+  slug: string,
+  id: number
+}
+
+const Voucher:FunctionComponent<VoucherProps> = ({ slug, id}) =>{
   const { error, loading, data } = useQuery(GET_OFFER_BY_SLUG, { variables: { slug }, fetchPolicy: 'cache-and-network' });
   return (
     <>
